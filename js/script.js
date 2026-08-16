0aed9110645a1e643821f1a32c3ded2abf6e769f
fazil203 <mohammedfazil2k03@gmail.com>
2026-08-16 21:17:04 +0400

    java script

--- js/script.js
+++ js/script.js
@@ -1,2 +1,33 @@
-let name = "Fazil";
-console.log(name);
+console.log("JavaScript connected!");
+
+const menuBtn = document.querySelector("#menuBtn");
+const nav = document.querySelector("nav");
+const closeBtn = document.querySelector("#closeBtn");
+const overlay = document.querySelector("#overlay");
+const navLinks = document.querySelectorAll("nav a");
+
+
+menuBtn.addEventListener("click", function() {
+    nav.classList.toggle("active");
+    overlay.classList.toggle("active");
+});
+
+
+closeBtn.addEventListener("click", function() {
+    nav.classList.remove("active");
+    overlay.classList.remove("active");
+});
+
+
+navLinks.forEach(function(link) {
+    link.addEventListener("click", function() {
+        nav.classList.remove("active");
+        overlay.classList.remove("active");
+    });
+});
+
+
+overlay.addEventListener("click", function() {
+    nav.classList.remove("active");
+    overlay.classList.remove("active");
+});
