255296d0a132d6ad70e532213705e4b9f1c7bd55
fazil203 <mohammedfazil2k03@gmail.com>
2026-08-16 22:54:47 +0400

    new css updations

--- js/script.js
+++ js/script.js
@@ -5,29 +5,32 @@ const nav = document.querySelector("nav");
 const closeBtn = document.querySelector("#closeBtn");
 const overlay = document.querySelector("#overlay");
 const navLinks = document.querySelectorAll("nav a");
+const logoHome = document.querySelector("Al Yaseen Logo");
 
+function closeMenu() {
+    nav.classList.remove("active");
+    overlay.classList.remove("active");
+}
 
 menuBtn.addEventListener("click", function() {
     nav.classList.toggle("active");
     overlay.classList.toggle("active");
 });
 
-
 closeBtn.addEventListener("click", function() {
-    nav.classList.remove("active");
-    overlay.classList.remove("active");
+    closeMenu();
 });
 
+overlay.addEventListener("click", function() {
+    closeMenu();
+});
 
 navLinks.forEach(function(link) {
     link.addEventListener("click", function() {
-        nav.classList.remove("active");
-        overlay.classList.remove("active");
+        closeMenu();
     });
 });
 
-
-overlay.addEventListener("click", function() {
-    nav.classList.remove("active");
-    overlay.classList.remove("active");
-});
+logoHome.addEventListener("click",function(){
+    
+})
