8b6360c3b3c71dbe8bb67b9d384e1ceb0ac26d87
fazil203 <mohammedfazil2k03@gmail.com>
2026-08-18 22:43:08 +0400

    js form

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ const closeBtn = document.querySelector("#closeBtn");
 const overlay = document.querySelector("#overlay");
 const navLinks = document.querySelectorAll("nav a");
 const serviceButtons = document.querySelectorAll(".service-btn");
+const enquiryForm = document.querySelector("#enquiryForm");
 
 function closeMenu() {
     nav.classList.remove("active");
@@ -41,13 +42,28 @@ serviceButtons.forEach(function(button) {
 
         const encodedMessage = encodeURIComponent(message);
 
-        const whatsappUrl = `https://wa.me/971567863568?text=${encodedMessage}`;
+        const whatsappUrl = `https://wa.me/971547863568?text=${encodedMessage}`;
 
-        console.log(whatsappUrl);
-
-        console.log(encodedMessage);
-
+        window.open(whatsappUrl,"_blank");
 
     });
 
+});
+
+enquiryForm.addEventListener("submit",function(event){
+    event.preventDefault();
+
+    const name = document.querySelector("#customerName").value;
+    const phone = document.querySelector("#customerPhone").value;
+    const message = document.querySelector("#customerMessage").value;
+    const enquiryMessage = `Hello AL YASEEN,
+        
+        Name: ${name}
+        Phone: ${phone}
+        Message: ${message}`;
+
+    const encodedMessage = encodeURIComponent(enquiryMessage);
+    const whatsappUrl = `https://wa.me/971547863568?text=${encodedMessage}` ;
+
+    window.open(whatsappUrl, "_blank");
 });
